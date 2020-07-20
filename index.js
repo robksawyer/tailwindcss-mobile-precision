162e8dbe8df40ce32c802b41fd9a480e7bc17421
Rob Sawyer <robksawyer@gmail.com>
2020-07-20 15:37:37 +0900

    Updates to the syntax

--- index.js
+++ index.js
@@ -23,61 +23,79 @@ module.exports = plugin(
         screens: {
           psm: {
             // iPhone 5/SE
-            min: '320px', max: '568px'
+            raw: 'screen and (min-width: 320px) and (max-width: 568px)',
           },
           psmp: {
             // iPhone 5/SE
             // Portrait
             raw:
-              '(min-device-width:320px),(max-device-width:568px),(orientation: portrait)',
+              'screen and (min-device-width:320px) and (max-device-width:568px) and (orientation:portrait)',
           },
           psml: {
             // iPhone 5/SE
             // Landscape
             raw:
-              '(min-device-width:320px),(max-device-width:568px),(orientation: landscape)',
+              'screen and (min-device-width:320px) and (max-device-width:568px) and (orientation:landscape)',
+          },
+          pxl: {
+            // iPhone X
+            // Portrait and Landscape
+            raw:
+              'screen and (device-width:375px) and (device-height:812px) and (-webkit-device-pixel-ratio:3)',
+          },
+          pxlp: {
+            // iPhone X
+            // Portrait
+            raw:
+              'screen and (device-width:375px) and (device-height:812px) and (orientation:portrait) and (-webkit-device-pixel-ratio:3)',
+          },
+          pxll: {
+            // iPhone X
+            // Landscape
+            raw:
+              'screen and (device-width:375px) and (device-height:812px) and (orientation:landscape) and (-webkit-device-pixel-ratio:3)',
           },
           tsm: {
             // iPad 1, 2, Mini and Air
             // Portrait and Landscape
             raw:
-              '(min-device-width:768px),(max-device-width:1024px),(-webkit-min-device-pixel-ratio:1)',
+              'screen and (min-device-width:768px) and (max-device-width:1024px),(-webkit-min-device-pixel-ratio:1)',
           },
           tsmp: {
             // iPad 1, 2, Mini and Air
             // Portrait
             raw:
-              '(min-device-width:768px),(max-device-width:1024px),(orientation:portrait),(-webkit-min-device-pixel-ratio:1)',
+              'screen and (min-device-width:768px) and (max-device-width:1024px) and (orientation:portrait) and (-webkit-min-device-pixel-ratio:1)',
           },
           tsml: {
             // iPad 1, 2, Mini and Air
             // Landscape
             raw:
-              '(min-device-width:768px),(max-device-width:1024px),(orientation:landscape),(-webkit-min-device-pixel-ratio:1)',
+              'screen and (min-device-width:768px) and (max-device-width:1024px) and (orientation:landscape) and (-webkit-min-device-pixel-ratio:1)',
           },
           tmd: {
             // iPad 3, 4 and Pro 9.7"
             // Portrait and Landscape
             raw:
-              '(min-device-width:768px),(max-device-width:1024px),(-webkit-min-device-pixel-ratio:2)',
+              'screen and (min-device-width:768px) and (max-device-width:1024px) and (-webkit-min-device-pixel-ratio:2)',
           },
           tmdp: {
             // iPad 3, 4 and Pro 9.7"
             // Portrait
             raw:
-              '(min-device-width:768px),(max-device-width:1024px),(orientation:portrait),(-webkit-min-device-pixel-ratio:2)',
+              'screen and (min-device-width:768px) and (max-device-width:1024px) and (orientation:portrait) and (-webkit-min-device-pixel-ratio:2)',
           },
           tmdl: {
             // iPad 3, 4 and Pro 9.7"
             // Landscape
             raw:
-              '(min-device-width:768px),(max-device-width:1024px),(orientation:landscape),(-webkit-min-device-pixel-ratio:2)',
+              'screen and (min-device-width:768px) and (max-device-width:1024px) and (orientation:landscape) and (-webkit-min-device-pixel-ratio:2)',
           },
           tlg: {
             // iPad Pro 10.5"
             // Portrait and Landscape
             raw:
-              '(min-device-width:834px),(max-device-width:1112px),(-webkit-min-device-pixel-ratio:2)',
+              'screen and (min-device-width:834px) and (max-device-width:1112px) and (-webkit-min-device-pixel-ratio:2)',
           },
           tlgp: {
             // iPad Pro 10.5"
@@ -85,7 +103,7 @@ module.exports = plugin(
             // Declare the same value for min- and max-width to avoid colliding with desktops
             // Source: https://medium.com/connect-the-dots/css-media-queries-for-ipad-pro-8cad10e17106
             raw:
-              '(min-device-width:834px),(max-device-width:834px),(orientation:portrait),(-webkit-min-device-pixel-ratio:2)',
+              'screen and (min-device-width:834px) and (max-device-width:834px) and (orientation:portrait) and (-webkit-min-device-pixel-ratio:2)',
           },
           tlgl: {
             // iPad Pro 10.5"
@@ -93,7 +111,7 @@ module.exports = plugin(
             // Declare the same value for min- and max-width to avoid colliding with desktops
             // Source: https://medium.com/connect-the-dots/css-media-queries-for-ipad-pro-8cad10e17106
             raw:
-              '(min-device-width:1112px),(max-device-width:1112px),(orientation:landscape),(-webkit-min-device-pixel-ratio:2)',
+              'screen and (min-device-width:1112px) and (max-device-width:1112px) and (orientation:landscape) and (-webkit-min-device-pixel-ratio:2)',
           },
           txl: {
             // iPad Pro 12.9"
@@ -101,7 +119,7 @@ module.exports = plugin(
             // Declare the same value for min- and max-width to avoid colliding with desktops
             // Source: https://medium.com/connect-the-dots/css-media-queries-for-ipad-pro-8cad10e17106
             raw:
-              '(min-device-width:1024px),(max-device-width:1366px),(-webkit-min-device-pixel-ratio:2)',
+              'screen and (min-device-width:1024px) and (max-device-width:1366px) and (-webkit-min-device-pixel-ratio:2)',
           },
           txlp: {
             // iPad Pro 12.9"
@@ -109,7 +127,7 @@ module.exports = plugin(
             // Declare the same value for min- and max-width to avoid colliding with desktops
             // Source: https://medium.com/connect-the-dots/css-media-queries-for-ipad-pro-8cad10e17106
             raw:
-              '(min-device-width:1024px),(max-device-width:1024px),(orientation:portrait),(-webkit-min-device-pixel-ratio:2)',
+              'screen and (min-device-width:1024px) and (max-device-width:1024px) and (orientation:portrait) and (-webkit-min-device-pixel-ratio:2)',
           },
           txll: {
             // iPad Pro 12.9"
@@ -117,7 +135,7 @@ module.exports = plugin(
             // Declare the same value for min- and max-width to avoid colliding with desktops
             // Source: https://medium.com/connect-the-dots/css-media-queries-for-ipad-pro-8cad10e17106
             raw:
-              '(min-device-width:1366px),(max-device-width:1366px),(orientation:landscape),(-webkit-min-device-pixel-ratio:2)',
+              'screen and (min-device-width:1366px) and (max-device-width:1366px) and (orientation:landscape) and (-webkit-min-device-pixel-ratio:2)',
           },
         },
       },
